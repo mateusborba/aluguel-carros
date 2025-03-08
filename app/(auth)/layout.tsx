6596a65bc896f182475d777f128cb981eb9aa789
Mateus Borba <mateusborba@Mateuss-MacBook-Air.local>
2025-03-07 22:44:41 -0300

    fix: corrige layout de auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   return (
     <main className="h-screen w-full items-center justify-center flex">
-      <div className="bg-white w-full h-full hidden lg:flex flex-col justify-between items-center py-10 border-r border-sidebar-foreground">
+      <div className="bg-foreground w-full h-full hidden lg:flex flex-col justify-between items-center py-10 border-r border-sidebar-foreground">
         <div />
         <div className="flex flex-col gap-2 items-center -mt-10">
           <div className="flex flex-col gap-2 items-center">
@@ -17,7 +17,7 @@ const AuthLayout = async ({ children }: { children: ReactNode }) => {
         </span>
       </div>
       <div className="w-full flex justify-center gap-20 h-full flex-col px-10 lg:p-0 items-center">
-        <div className="md:hidden flex flex-col gap-2 -mt-20 justify-center items-center">
+        <div className="md:hidden md:-mt-20 flex flex-col gap-2 justify-center items-center">
           <Car className="size-20" />
           <h1 className="text-3xl text-white font-bold">Rental Cars</h1>
         </div>
